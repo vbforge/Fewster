c74d279619fe7a8b5e681af9fe68ab08ec210568
vbforge <vladbogdantsev@gmail.com>
2025-09-01 00:28:52 +0300

    test example add

--- src/test/java/com/vladproduction/fewster/FewsterApplicationTests.java
+++ src/test/java/com/vladproduction/fewster/FewsterApplicationTests.java
@@ -2,8 +2,10 @@ package com.vladproduction.fewster;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FewsterApplicationTests {
 
     @Test
